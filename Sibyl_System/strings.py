628b5e885ecc24f3d65bf432f057e5667df2cf49
UNKNOWN <84973544+Unknown-San@users.noreply.github.com>
2022-01-01 18:40:39 -0800

    Update strings.py

--- Sibyl_System/strings.py
+++ Sibyl_System/strings.py
@@ -1,7 +1,7 @@
 on_string = """
-Connection successful!
-Welcome {Enforcer} {name}
-You are a valid user!
+✯ Connection successful! ✯
+ ╘ Welcome {Enforcer} {name} ✯
+ ╘ You are a valid user! ✯
 """
 
 # Make sure not to change these too much
